00d9031dff257719501f60600c2b5c4249cae021
Lucas Borges <lucas_borges_br@hotmail.com>
2021-12-16 23:18:27 -0300

    configurando novo repositorio

---
+++ elibras/src/App.js
@@ -0,0 +1,26 @@
+import Vlibras from "@djpfs/react-vlibras";
+
+
+function App() {
+  return (
+    <div className="App">
+    <Vlibras/>
+      <header className="App-header">
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
