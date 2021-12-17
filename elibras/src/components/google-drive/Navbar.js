a327b2e6c1fcba3a9947a7158d77c6003228b490
Lucas Borges <lucas_borges_br@hotmail.com>
2021-12-16 23:59:35 -0300

    elibras drive concluido

---
+++ elibras/src/components/google-drive/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Navbar, Nav } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
+export default function NavbarComponent() {
+  return (
+    <Navbar bg="light" expand="sm">
+      <Navbar.Brand as={Link} to="/">
+        Elibras Drive
+      </Navbar.Brand>
+      <Nav>
+        <Nav.Link as={Link} to="/user">
+          Perfil
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  )
+}
